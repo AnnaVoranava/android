b09a4073dcfc02a88960e5357f13b8e1a9aa51f1
AnnaVoranava <afonna85@gmail.com>
2023-10-08 14:26:49 +0300

    Initial

--- app/src/main/java/by/iba/firstapp/myquiz/MainActivity.kt
+++ app/src/main/java/by/iba/firstapp/myquiz/MainActivity.kt
@@ -1,42 +1,95 @@
 package by.iba.firstapp.myquiz
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlin.jvm.internal.Intrinsics.Kotlin
 
 class MainActivity : AppCompatActivity() {
     private lateinit var quizItemList: List<QuizItem>
     private lateinit var startButton: Button
+    private var currentQuizItem: QuizItem? = null
     private var activeQuiz: MutableMap<QuizItem, AnswerType>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         quizItemList = createQuestionList()
+
         startButton = findViewById(R.id.btn_quiz_start)
         startButton.setOnClickListener {
-            if (activeQuiz == null) {
-                startNewQuiz()
-            } else {
-                TODO()
-            }
-            var currentQuizItem:QuizItem?=null
+            checkNextOrFinish()
+        }
 
-            activeQuiz!!.forEach { (key, value) ->
-                if (value == AnswerType.UNDEFINED) {
-                    currentQuizItem = key
-                }
-            }
+    }
+
+
+    private fun checkNextOrFinish() {
+        if (activeQuiz == null) {
+            startNewQuiz()
+        }
+        activeQuiz!!.forEach() { (key, value) ->
+            if (value == AnswerType.UNDEFINED) currentQuizItem = key
+        }
+        if (currentQuizItem == null) {
+            showResult()
+        } else {
             val newIntent = Intent(applicationContext, QuestionActivity::class.java)
             newIntent.putExtra(IntentKeys.KEY_QUESTION, currentQuizItem!!.question)//TODO
             newIntent.putExtra(IntentKeys.KEY_ANSWER0, currentQuizItem!!.answer.get(0))
             newIntent.putExtra(IntentKeys.KEY_ANSWER1, currentQuizItem!!.answer.get(1))
             newIntent.putExtra(IntentKeys.KEY_CORRECT_INDEX, currentQuizItem!!.correctId)
             startActivity(newIntent)
+            val activityLauncher =
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                    if (result.resultCode == Activity.RESULT_OK) {
+                        val incomingIntent = result.data
+                        val isCorrect =
+                            incomingIntent?.getBooleanExtra(IntentKeys.KEY_RESULT, false) ?: false
+                        activeQuiz?.put(
+                            currentQuizItem!!,
+                            if (isCorrect) AnswerType.CORRECT else AnswerType.INCORRECT
 
+                        )
+                        currentQuizItem = null
+                        checkNextOrFinish()
+                    }
+                }
         }
+    }
+
+    private fun showResult() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "MainActivity onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "MainActivity onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "MainActivity onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "MainActivity onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "MainActivity onDestroy")
     }
 
     private fun startNewQuiz() {
@@ -47,6 +100,7 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+
 data class QuizItem(val question: String, val answer: List<String>, val correctId: Int)
 
 fun createQuestionList(): List<QuizItem> {
@@ -54,7 +108,7 @@ fun createQuestionList(): List<QuizItem> {
     quizItemList.add(QuizItem("Котлин - статический язык?", listOf("Да", "Нет"), 0))
     quizItemList.add(QuizItem("Сколько примитивных типов в Котлин?", listOf("0", "8"), 1))
     quizItemList.add(QuizItem("Является ли Котлин функциональным языком?", listOf("Да", "Нет"), 1))
-    quizItemList.add(QuizItem("Чем являются функции в Котлин,", listOf("Объект", "Переменная"), 0))
+    quizItemList.add(QuizItem("Чем являются функции в Котлин?", listOf("Объект", "Переменная"), 0))
     return quizItemList
 }
 
@@ -65,10 +119,11 @@ enum class AnswerType {
 class IntentKeys {
 
     companion object {
-       val KEY_QUESTION: String ="question"
-        val KEY_ANSWER0: String ="answer0"
-        val KEY_ANSWER1: String ="answer1"
-        val KEY_CORRECT_INDEX: String ="correctIndex"
+        val KEY_QUESTION: String = "question"
+        val KEY_ANSWER0: String = "answer0"
+        val KEY_ANSWER1: String = "answer1"
+        val KEY_CORRECT_INDEX: String = "correctIndex"
+        val KEY_RESULT = "KEY_RESULT"
 
     }
 }
--- app/src/main/java/by/iba/firstapp/myquiz/QuestionActivity.kt
+++ app/src/main/java/by/iba/firstapp/myquiz/QuestionActivity.kt
@@ -1,17 +1,95 @@
 package by.iba.firstapp.myquiz
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import by.iba.firstapp.myquiz.R.id.btn_quiz_start
 
 class QuestionActivity : AppCompatActivity() {
+    lateinit var submitButton: Button
+    lateinit var questionView: TextView
+    lateinit var answer1View: TextView
+    lateinit var answer2View: TextView
+    lateinit var card1View: CardView
+    lateinit var card2View: CardView
+    private var result=false
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
-        val question=intent.getStringExtra(IntentKeys.KEY_QUESTION)//TODO
-        val answer0=intent.getStringExtra(IntentKeys.KEY_ANSWER0)
-        val answer1=intent.getStringExtra(IntentKeys.KEY_ANSWER1)
-        val correctIndex=intent.getIntExtra(IntentKeys.KEY_CORRECT_INDEX,-1)
-        Log.d("MyTag","question: $question, answer0: $answer0, answer1: $answer1, correctIndex: $correctIndex")   }
+        submitButton = findViewById(R.id.btn_submit)
+        questionView = findViewById(R.id.tv_question)
+        answer1View = findViewById(R.id.tv_answer_1)
+        answer2View = findViewById(R.id.tv_answer_2)
+        card1View = findViewById(R.id.cv_answer_1)
+        card2View = findViewById(R.id.cv_answer_2)
+        submitButton.isEnabled=false
+
+
+        val question = intent.getStringExtra(IntentKeys.KEY_QUESTION)//TODO
+        val answer0 = intent.getStringExtra(IntentKeys.KEY_ANSWER0)
+        val answer1 = intent.getStringExtra(IntentKeys.KEY_ANSWER1)
+        val correctIndex = intent.getIntExtra(IntentKeys.KEY_CORRECT_INDEX, -1)
+        Log.d(
+            QuestionActivity::class.java.simpleName,
+            "question: $question, answer0: $answer0, answer1: $answer1, correctIndex: $correctIndex"
+        )
+
+        questionView.text = question?.let { it }?:"Error"
+        answer1View.text=answer0
+        answer2View.text=answer1
+
+        answer1View.setOnClickListener{
+            card1View.elevation=0F
+            card2View.elevation=10F
+            result = correctIndex == 0
+            submitButton.isEnabled=true
+        }
+        answer2View.setOnClickListener{
+            card1View.elevation=10F
+            card2View.elevation=0F
+            result = correctIndex == 1
+            submitButton.isEnabled=true
+        }
+        submitButton.setOnClickListener {
+            intent= Intent(applicationContext, MainActivity::class.java)
+            intent.putExtra(IntentKeys.KEY_RESULT, result)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+
+        }
+        Log.d("Lifecycle", "QuestionActivity onCreate")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "MainActivity onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "QuestionActivity onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "QuestionActivity onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "QuestionActivity onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "QuestionActivity onDestroy")
+    }
 }
 
